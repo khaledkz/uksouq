bb94812579a8c61904f6a31653de9bf053dea067
kharazzoro <kha.1991.14.2017@gmail.com>
2018-04-15 04:05:08 +0100

    app require and use routes

--- server/app.js
+++ server/app.js
@@ -5,7 +5,9 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+ const product = require('./routes/product/product');
+const user = require('./routes/user/user');
+const mainpage = require('./routes/mainpage/mainpage');
 
 var app = express();
 
@@ -20,7 +22,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/users', user);
+app.use('/products',product)
+app.use('/mainpage',mainpage)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

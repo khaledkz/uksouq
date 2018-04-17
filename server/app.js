255e02b4c3579b7e258c119d7e1ee8142ae7099c
kharazzoro <kha.1991.14.2017@gmail.com>
2018-04-17 17:51:53 +0100

    import category routes

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
  const product = require('./routes/product/product');
 const user = require('./routes/user/user');
 const mainpage = require('./routes/mainpage/mainpage');
+const category = require('./routes/Category/Categories');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/user', user);
 app.use('/product',product)
 app.use('/mainpage',mainpage)
+app.use('/category',category)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

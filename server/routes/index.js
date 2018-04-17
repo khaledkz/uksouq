848a6e5f85f837bca85d963897e33cb475666bda
kharazzoro <kha.1991.14.2017@gmail.com>
2018-04-17 16:45:37 +0100

    test db

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const dbClient = require('./../mongodb/dbclient/Product')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  cb=()=>{
+    res.render('index', { title: 'Express' });
+  }
+  dbClient.addProduct({title:'macbook pro',price:400},cb)
 });
 
 module.exports = router;

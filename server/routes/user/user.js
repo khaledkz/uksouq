d5cc5dd8e413437f599a5b639e0a2c0f32c33ef8
kharazzoro <kha.1991.14.2017@gmail.com>
2018-04-18 00:21:10 +0100

    User add find all router

--- server/routes/user/user.js
+++ server/routes/user/user.js
@@ -1,24 +1,32 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const UserDB=require('../../mongodb/dbclient/User');
+const UserDB = require("../../mongodb/dbclient/User");
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('user/menu');
+router.get("/", function(req, res, next) {
+  res.render("user/menu");
 });
 
-router.get('/edit', function(req, res, next) {
-  res.render('user/searchMenu');
+router.get("/edit", function(req, res, next) {
+  res.render("user/searchMenu");
 });
 
-router.get('/add', (req, res, next) => {
-   res.render('user/add');
+router.get("/add", (req, res, next) => {
+  res.render("user/add");
 });
- 
-router.post('/add', (req, res, next) => {
-  cb=()=>{
-      res.redirect('/')
-  }
-  UserDB.addUser(req.body,cb)
- });
+
+router.post("/add", (req, res, next) => {
+  cb = () => {
+    res.redirect("/");
+  };
+  UserDB.addUser(req.body, cb);
+});
+
+router.get("/edit/all", (req, res, next) => {
+  cb = data => {
+    console.log(data);
+    res.render("user/list", { data });
+  };
+  UserDB.findUser(cb);
+});
 
 module.exports = router;

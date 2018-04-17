36d8b5360c942e69bff0b0a5a0b8441a88720a7c
kharazzoro <kha.1991.14.2017@gmail.com>
2018-04-17 23:22:28 +0100

    add product post ruter

--- server/routes/product/product.js
+++ server/routes/product/product.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-
+const productDB=require('../../mongodb/dbclient/Product')
 /* GET home page. */
 router.get('/', (req, res, next) => {
   res.render('product/menu');
@@ -13,6 +13,14 @@ router.get('/edit', (req, res, next) => {
 router.get('/add', (req, res, next) => {
   res.render('product/add');
 });
- 
+
+
+router.post('/add', (req, res, next) => {
+  cb=()=>{
+    return res.redirect('/')
+  }
+  productDB.addProduct(req.body,cb)
+ });
+
 
 module.exports = router;

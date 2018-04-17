508e8ea1cd13a30c1c9cd377062b949630e29d01
kharazzoro <kha.1991.14.2017@gmail.com>
2018-04-17 23:21:52 +0100

    router add category post router

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,13 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const dbClient = require('./../mongodb/dbclient/Product')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  cb=()=>{
-    res.render('index', { title: 'Express' });
-  }
-  dbClient.addProduct({title:'macbook pro',price:400},cb)
-});
+     res.render('index', { title: 'Express' }); 
+ });
 
 module.exports = router;

8c60a3799c62d63841146c6819e78925bda5717c
kharazzoro <kha.1991.14.2017@gmail.com>
2018-04-18 01:46:40 +0100

    server router single product

--- server/routes/product/product.js
+++ server/routes/product/product.js
@@ -1,33 +1,32 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-const productDB=require('../../mongodb/dbclient/Product')
+const productDB = require("../../mongodb/dbclient/Product");
 /* GET home page. */
-router.get('/', (req, res, next) => {
-  res.render('product/menu');
+router.get("/", (req, res, next) => {
+  res.render("product/menu");
 });
-
-router.get('/edit', (req, res, next) => {
-  res.render('product/searchMenu');
+router.get("/edit", (req, res, next) => {
+  res.render("product/searchMenu");
 });
-
-router.get('/add', (req, res, next) => {
-  res.render('product/add');
+router.get("/add", (req, res, next) => {
+  res.render("product/add");
 });
-
-
-router.post('/add', (req, res, next) => {
-  cb=()=>{
-    return res.redirect('/')
-  }
-  productDB.addProduct(req.body,cb)
- });
-
- router.get("/edit/all", (req, res, next) => {
+router.post("/add", (req, res, next) => {
+  cb = () => {
+    return res.redirect("/");
+  };
+  productDB.addProduct(req.body, cb);
+});
+router.get("/edit/all", (req, res, next) => {
   cb = data => {
     res.render("product/list", { data });
   };
   productDB.findProduct(cb);
 });
-
+router.get("/edit/all/:id", (req, res, next) => {
+  const { id } = req.params;
+  cb = data => res.render("product/single", { data });
+  productDB.findProductById(id, cb);
+});
 
 module.exports = router;
